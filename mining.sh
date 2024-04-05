cd5b470c702926d473f9085d23509683432056de
kgdmzz <43299860+kgdmzz@users.noreply.github.com>
2024-04-05 13:11:59 +0700

    Update mining.sh

--- mining.sh
+++ mining.sh
@@ -3,7 +3,7 @@ while true
 do
   echo "Memulai proses menambang..."
   ore \
-    --rpc https://zksync-mainnet.g.alchemy.com/v2/0Z48roygdz28JamOezCcXV86RLuRMgFB \
+    --rpc https://solana-mainnet.g.alchemy.com/v2/N1BJSXnxFcTZx1nVP9f-8okhWaaj08cp \
     --keypair ~/.config/solana/id.json \
     --priority-fee 5000000 \
     mine \
